2727551e887614bfcf538655db035f5e50deb732
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-24 19:03:06 +0300

    Changed 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+      let task = true;
+      let task2 = false;
     }
   
     return [task, task2];
-  }
+}

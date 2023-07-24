be95bc60587bbc74ac250738c7ba44cadb8b4a7e
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-24 21:07:34 +0300

    updated 1-block-scoped.js

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2015, // Or the appropriate ECMAScript version you are using
+    sourceType: 'module',
+  },
+  // Your other ESLint rules and configurations can go here
+};
+

18112499ac16534b00f9ebfe67e9861aa5e7ff9d
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-24 23:07:08 +0300

    fixed eslint error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,8 +5,9 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     //eslint-disable-next-line
     const task = true;
+    //eslint-disable-next-line
     const task2 = false;
   }
 
   return [task, task2];
-}
+}       

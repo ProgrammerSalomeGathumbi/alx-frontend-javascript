205e494de674c4f29f4d4ec8448360addecc0e5a
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-26 15:24:13 +0300

    Added 1-block-scoped.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,8 +1,36 @@
 module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 2015, // Or the appropriate ECMAScript version you are using
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  // Your other ESLint rules and configurations can go here
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
 

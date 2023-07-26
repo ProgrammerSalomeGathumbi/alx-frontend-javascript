c0c156cf9692ba5d50fcc35dbf6c3a1b4bcb3872
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-26 19:07:28 +0300

    fixed erros in 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return(`${departmentName}: [ ${employees}]`);
+  const dep = { [departmentName]: employees };
+  return dep;
 }

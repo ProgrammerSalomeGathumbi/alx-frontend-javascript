a84be4c38258f2b01888fdbda7986ba4f1c75a99
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-27 18:31:27 +0300

    fixed eslint errors in 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const rooms = [19, 20, 34];
-    const classrooms = sizes.map((size) => new ClassRoom(size));
-    return classrooms;
+  const rooms = [19, 20, 34];
+  const classrooms = rooms.map((room) => new ClassRoom(room));
+  return classrooms;
 }

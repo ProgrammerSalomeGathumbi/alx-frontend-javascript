b8a82b2a6b3383aa968d2e3f2ce3e7de4bd25223
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-27 23:46:41 +0300

    updated 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -26,7 +26,9 @@ export class StudentHolberton {
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
@@ -39,5 +41,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-
 export default listOfStudents;

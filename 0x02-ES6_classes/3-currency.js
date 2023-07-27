541d21b0597c0a470fa7e7abc612e80e8ab91e79
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-07-27 20:12:38 +0300

    updated 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,2 +1,25 @@
-export default class Currency(){
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
